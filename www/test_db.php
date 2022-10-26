3d42832409b0bdb86e66bad867f216e4694a9e9d
ppharrington <pierre-paul.harrington@ac-versailles.fr>
2022-10-26 08:32:47 +0200

    test db files overwritten

--- www/test_db.php
+++ www/test_db.php
@@ -1,13 +1,19 @@
 <?php
+$testTitle="MySQLi Connection test";
+include_once 'assets/htmltags.php';
 $link = mysqli_connect("database", "root", $_ENV['MYSQL_ROOT_PASSWORD'], null);
+echo $headtags;
 
 if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "<h2 class='subtitle no-bottom-margin'>FAILURE</h2><p>Unable to connect to MySQL.</p>" . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    echo $foottags;
     exit;
 }
 
-echo "Success: A proper connection to MySQL was made! The docker database is great." . PHP_EOL;
+echo "<h2 class='subtitle no-bottom-margin'>SUCCESS</h2><p>Connection to MySQL was made.</p>" . PHP_EOL;
+
+echo $foottags;
 
 mysqli_close($link);
--- www/test_db_pdo.php
+++ www/test_db_pdo.php
@@ -1,15 +1,19 @@
 <?php
-
+$testTitle="PDO Connection test";
+include 'assets/htmltags.php';
 $DBuser = 'root';
 $DBpass = $_ENV['MYSQL_ROOT_PASSWORD'];
 $pdo = null;
 
+echo $headtags;
+
 try{
     $database = 'mysql:host=database:3306';
     $pdo = new PDO($database, $DBuser, $DBpass);
-    echo "Success: A proper connection to MySQL was made! The docker database is great.";    
+    echo "<h2 class='subtitle no-bottom-margin'>SUCCESS</h2><p>Connection to MySQL with PDO was made.</p>";    
 } catch(PDOException $e) {
-    echo "Error: Unable to connect to MySQL. Error:\n $e";
+    echo "<h2 class='subtitle no-bottom-margin'>FAILURE</h2><p>Unable to connect to MySQL with PDO.</p><p>Error:\n $e</p>";
 }
 
+echo $foottags;
 $pdo = null;
